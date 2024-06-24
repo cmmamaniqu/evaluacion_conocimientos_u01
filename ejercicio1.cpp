9f36dc54faa9ffddb5e58dde7d779caad8f7e76c
cmmamaniqu <cmmamaniquQ@unjbg.edu.pe>
2024-06-24 01:52:28 -0500

    Actualización del ejercicio1: añiendo condición para la variable b dentro del operador

---
+++ ejercicio1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    char op;
+    cin>>a;
+    cin>>b;
+    cin>>op;
+    switch(op){
+        case '+':
+          cout<<"r"<<a+b;
+        break;
+        case'-':
+         cout<<"r"<<a-b;
+        break;
+        case'*':
+          cout<<"r"<<a*b;
+        break;
+        case'/':
+          cout<<"r"<<a/b;
+          
+        break;
+        }
+        return 0;
+}
